50eaf974de0c9700c4ac688b6e78d4d0bf7c3aa3
Mohnish <mohnishpwr10@gmail.com>
2022-06-16 02:48:38 -0400

    Changes have been committed.

---
+++ MyFirstServlet/src/main/java/com/simplilearn/demo/WelcomeServlet.java
@@ -0,0 +1,5 @@
+package com.simplilearn.demo;
+
+public class WelcomeServlet extends GenericServlet{
+
+}
---
+++ MyServlet/src/main/java/com/simplilearn/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package com.simplilearn.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
